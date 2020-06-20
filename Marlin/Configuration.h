355cca9ccefd6a47a98d56311f2593025d58508d
av8er <av8er@cox.net>
2020-06-20 16:54:07 -0700

    Update Configuration.h
    Update acceleration and jerk settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -759,7 +759,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 800, 500, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 500, 5000 }   //   { 500, 800, 500, 5000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -774,9 +774,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          500    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  500    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   500    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves 500
+#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts 500
+#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves  500
 
 /**
  * Default Jerk limits (mm/s)
@@ -790,7 +790,7 @@
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 7.0 // 10
   #define DEFAULT_YJERK 7.0 //  10
-  #define DEFAULT_ZJERK  0.3
+  #define DEFAULT_ZJERK  0.4
 
   //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
 
